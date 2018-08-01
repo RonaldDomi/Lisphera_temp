59ab828e292870c567534cbe8ecb95d40eddb9b0
Ronald Domi <ronalddomi4@gmail.com>
2018-08-02 00:08:08 +0200

    basic structure

--- lisphera/__init__.py
+++ lisphera/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test123@localhost/flaskmovie'
+db = SQLAlchemy(app)
+
+
+
 
 from lisphera import routes
-
-#test
